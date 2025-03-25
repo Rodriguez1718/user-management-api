4c67448234020b5871d901f16e19453a38ce3d9f
unknown <YY342@ccs.local>
2025-03-25 21:30:06 +0800

    Adding A Creation Functionality

---
+++ src/routes/user.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import {
+  createUser
+} from "../controllers/userController";
+
+const router = Router();
+
+router.post("/users", createUser);
+
+
+export default router;
