b33ea5e798fbff0ddf72e85f88e37c963cd34beb
Rodriguez1718 <krisjohn.rodriguez@gmail.com>
2025-03-26 12:05:12 +0800

    fixed the issues, added hashing of password and some error handling

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { IsEmail, Length } from "class-validator";
 
 @Entity()
 export class User {
@@ -6,14 +7,18 @@ export class User {
   id!: number;
 
   @Column()
+  @Length(1, 50)
   firstName!: string;
 
   @Column()
+  @Length(1, 50)
   lastName!: string;
 
   @Column({ unique: true })
+  @IsEmail()
   email!: string;
 
   @Column()
+  @Length(6, 100)
   password!: string;
 }
